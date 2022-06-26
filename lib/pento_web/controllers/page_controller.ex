905d8247dc10cc5809ca9b12ab466753cbfabbd0
Papa Attrams <48452451+Attrams@users.noreply.github.com>
2022-06-26 12:15:19 +0000

    redirect user if logged in

--- lib/pento_web/controllers/page_controller.ex
+++ lib/pento_web/controllers/page_controller.ex
@@ -2,6 +2,11 @@ defmodule PentoWeb.PageController do
   use PentoWeb, :controller
 
   def index(conn, _params) do
-    render(conn, "index.html")
+    if conn.assigns[:current_user] do
+      IO.inspect(Routes.live_path(conn, PentoWeb.WrongLive))
+      conn |> redirect(to: Routes.live_path(conn, PentoWeb.WrongLive)) |> halt()
+    else
+      render(conn, "index.html")
+    end
   end
 end
