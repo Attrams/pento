7b82c85160541d1186daeb03e6d995856968f762
Papa Attrams <48452451+Attrams@users.noreply.github.com>
2022-07-20 20:41:47 +0000

    add the handle_info and handle_demographic_created function

--- lib/pento_web/live/survey_live.ex
+++ lib/pento_web/live/survey_live.ex
@@ -8,9 +8,20 @@ defmodule PentoWeb.SurveyLive do
     {:ok, socket |> assign_user(token)}
   end
 
+  @impl true
+  def handle_info({:created_demographic, demographic}, socket) do
+    {:noreply, handle_demographic_created(socket, demographic)}
+  end
+
   def assign_user(socket, token) do
     # IO.puts("Assign user with socket.private:")
     # IO.inspect(socket.private)
     assign_new(socket, :current_user, fn -> Accounts.get_user_by_session_token(token) end)
+  end
+
+  def handle_demographic_created(socket, demographic) do
+    socket
+    |> put_flash(:info, "Demographic created successfully")
+    |> assign(:demographic, demographic)
   end
 end
